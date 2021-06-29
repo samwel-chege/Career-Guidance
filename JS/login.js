8915b464484c32eb8f969d6a3ab96bb9b3fd0929
karuu-01 <brian.muchori@student.moringaschool.com>
2021-06-29 22:59:25 +0300

    add changes

---
+++ JS/login.js
@@ -0,0 +1,76 @@
+$("#signup").click(function() {
+  $("#first").fadeOut("fast", function() {
+  $("#second").fadeIn("fast");
+  });
+  });
+  
+  $("#signin").click(function() {
+  $("#second").fadeOut("fast", function() {
+  $("#first").fadeIn("fast");
+  });
+  });
+  
+  
+    
+           $(function() {
+             $("form[name='login']").validate({
+               rules: {
+                 
+                 email: {
+                   required: true,
+                   email: true
+                 },
+                 password: {
+                   required: true,
+                   
+                 }
+               },
+                messages: {
+                 email: "Please enter a valid email address",
+                
+                 password: {
+                   required: "Please enter password",
+                  
+                 }
+                 
+               },
+               submitHandler: function(form) {
+                 form.submit();
+               }
+             });
+           });
+           
+  
+  
+  $(function() {
+    
+    $("form[name='registration']").validate({
+      rules: {
+        firstname: "required",
+        lastname: "required",
+        email: {
+          required: true,
+          email: true
+        },
+        password: {
+          required: true,
+          minlength: 5
+        }
+      },
+      
+      messages: {
+        firstname: "Please enter your firstname",
+        lastname: "Please enter your lastname",
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 5 characters long"
+        },
+        email: "Please enter a valid email address"
+      },
+    
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
+  });
+  
